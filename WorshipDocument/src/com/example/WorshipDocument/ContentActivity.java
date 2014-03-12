48eb2edab730ae4463c1ac6753cc30ad198cfd57
Phạm Trần Trung <trungpt@qsoft.com.vn>
2014-03-12 23:51:56 +0700

    update

---
+++ WorshipDocument/src/com/example/WorshipDocument/ContentActivity.java
@@ -0,0 +1,20 @@
+package com.example.WorshipDocument;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Trung
+ * Date: 3/12/14
+ * Time: 11:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ContentActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
---
+++ WorshipDocument/src/com/example/WorshipDocument/MainACtivity.java
@@ -0,0 +1,20 @@
+package com.example.WorshipDocument;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Trung
+ * Date: three/12/14
+ * Time: 11:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
