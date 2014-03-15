268aaefbf94848ad0ce83279f62b89ab2802dedc
Phạm Trần Trung <trungpt@qsoft.com.vn>
2014-03-15 22:01:26 +0700

    update

--- WorshipDocument/src/com/example/WorshipDocument/GridAdapter.java
+++ WorshipDocument/src/com/example/WorshipDocument/GridAdapter.java
@@ -2,12 +2,15 @@ package com.example.WorshipDocument;
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,13 +30,23 @@ public class GridAdapter extends BaseAdapter
     private List<String> bitmapsFiles = new ArrayList<String>();
     private String dirFrom;
     Bitmap bitmap;
+    protected ImageLoader imageLoader = ImageLoader.getInstance();
+    DisplayImageOptions options;
 
     public GridAdapter(Context mContext, List<Bitmap> listBitmap, String dirFrom)
     {
         this.mContext = mContext;
         this.dirFrom = dirFrom;
         this.bitmapsFiles = listBitmap(dirFrom);
-
+        options = new DisplayImageOptions.Builder()
+                .showImageForEmptyUri(0)
+                .showImageOnFail(0)
+                .cacheInMemory(true)
+                .cacheOnDisc(true)
+                .considerExifParams(true)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .imageScaleType(ImageScaleType.IN_SAMPLE_INT)
+                .build();
 
     }
 
@@ -59,44 +72,11 @@ public class GridAdapter extends BaseAdapter
     public View getView(int position, View view, ViewGroup viewGroup)
     {
         ImageView imageView = new ImageView(mContext);
-        InputStream istr = null;
-        try
-        {
-            istr = mContext.getAssets().open(dirFrom + "/" + bitmapsFiles.get(position));
-//            try {
-//                BitmapFactory.Options options = new BitmapFactory.Options();
-//                options.inSampleSize = 4;
-//                bitmap = BitmapFactory.decodeFile(file, options);
-//            } catch (OutOfMemoryError e) {
-//                e.printStackTrace();
-//
-//                System.gc();
-//
-//                try {
-//                    bitmap = BitmapFactory.decodeFile(file);
-//                } catch (OutOfMemoryError e2) {
-//                    e2.printStackTrace();
-//                    // handle gracefully.
-//                }
-//            }
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-//        if (bitmap != null)
-//        {
-//            bitmap.recycle();
-//        }
-//        else
-//        {
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 3;
-        bitmap = BitmapFactory.decodeStream(istr, null, options);
+        imageLoader.init(ImageLoaderConfiguration.createDefault(mContext));
+        imageLoader.displayImage(bitmapsFiles.get(position),imageView,options);
         imageView.setImageBitmap(bitmap);
         imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
         imageView.setLayoutParams(new GridView.LayoutParams(120, 210));
-//        }
         return imageView;
     }
 
